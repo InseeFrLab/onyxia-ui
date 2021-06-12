b6efa18a9ac34d8cf91041da97be426cc98da2d4
garronej <joseph.garrone.gj@gmail.com>
2021-06-13 00:58:22 +0200

    Less strict TypoGraphy

--- src/lib/typography.ts
+++ src/lib/typography.ts
@@ -1,70 +1,81 @@
 import type { TypographyOptions as MuiTypographyOptions } from "@material-ui/core/styles/createTypography";
+import { id } from "tsafe/id";
+
+export type FontWeightProperty =
+    | FontWeightProperty.Globals
+    | FontWeightProperty.FontWeightAbsolute
+    | "bolder"
+    | "lighter";
+export declare namespace FontWeightProperty {
+    export type FontWeightAbsolute = number | "bold" | "normal";
+    export type Globals = "-moz-initial" | "inherit" | "initial" | "revert" | "unset";
+}
 
 export type TypographyOptionsBase = typeof defaultTypography;
 
 export const defaultTypography = {
     "fontFamily": '"Work Sans", sans-serif',
     "h1": {
-        "fontWeight": "bold",
+        "fontWeight": id<FontWeightProperty>("bold"),
         "fontSize": "60px",
         "lineHeight": 1.2,
     },
     "h2": {
-        "fontWeight": "bold",
+        "fontWeight": id<FontWeightProperty>("bold"),
         "fontSize": "36px",
         "lineHeight": 1.11,
     },
     "h3": {
-        "fontWeight": 500,
+        "fontWeight": id<FontWeightProperty>(500),
         "fontSize": "28px",
         "lineHeight": 1.29,
     },
     "h4": {
-        "fontWeight": "bold",
+        "fontWeight": id<FontWeightProperty>("bold"),
         "fontSize": "24px",
         "lineHeight": 1.33,
     },
     "h5": {
-        "fontWeight": 600,
+        "fontWeight": id<FontWeightProperty>(600),
         "fontSize": "20px",
         "lineHeight": 1.2,
     },
     "h6": {
-        "fontWeight": 500,
+        "fontWeight": id<FontWeightProperty>(500),
         "fontSize": "20px",
         "lineHeight": 1.2,
     },
     "subtitle1": {
-        "fontWeight": 500,
+        "fontWeight": id<FontWeightProperty>(500),
         "fontSize": "18px",
         "lineHeight": 1.1,
     },
     "subtitle2": {
-        "fontWeight": 500,
+        "fontWeight": id<FontWeightProperty>(500),
         "fontSize": "14px",
         "lineHeight": 1.2,
     },
     "body1": {
-        "fontWeight": "normal",
+        "fontWeight": id<FontWeightProperty>("normal"),
         "fontSize": "18px",
         "lineHeight": 1.1,
     },
     "body2": {
-        "fontWeight": "normal",
+        "fontWeight": id<FontWeightProperty>("normal"),
         "fontSize": "14px",
         "lineHeight": 1.1,
     },
     "button": {
-        "fontWeight": 600,
+        "fontWeight": id<FontWeightProperty>(600),
         "fontSize": "20px",
         "lineHeight": 1.2,
     },
     "caption": {
-        "fontWeight": "normal",
+        "fontWeight": id<FontWeightProperty>("normal"),
         "fontSize": "14px",
         "lineHeight": 1,
     },
-} as const;
+};
 
 export function createMuiTypographyOptions(params: {
     typography: TypographyOptionsBase;
@@ -75,5 +86,5 @@ export function createMuiTypographyOptions(params: {
         ...typography,
         "fontWeightRegular": "normal",
         "fontWeightMedium": 500,
-    } as const;
+    };
 }
