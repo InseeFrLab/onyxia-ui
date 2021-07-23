8ba58b5eb2415b777780a0b14e1b92c6ce50857a
garronej <joseph.garrone.gj@gmail.com>
2021-07-23 23:28:02 +0200

    Add PageHeader and SearchBar

---
+++ src/PageHeader.tsx
@@ -0,0 +1,138 @@
+import { memo } from "react";
+import { makeStyles, Text } from "./lib/ThemeProvider";
+import { useDomRect } from "powerhooks/useDomRect";
+import { createUseGlobalState } from "powerhooks/useGlobalState";
+import { id } from "tsafe/id";
+import { useConstCallback } from "powerhooks/useConstCallback";
+import { createIcon } from "./Icon";
+import { createIconButton } from "./IconButton";
+import type { IconProps } from "./Icon";
+import type { FC } from "react";
+import CloseSharp from "@material-ui/icons/CloseSharp";
+
+export type PageHeaderProps<IconId extends string> = {
+    mainIcon?: IconId;
+    title: string;
+    helpIcon?: IconId;
+    helpTitle: NonNullable<React.ReactNode>;
+    helpContent: NonNullable<React.ReactNode>;
+    className?: string;
+};
+
+const useStyles = makeStyles<{ helperHeight: number }>()(
+    (theme, { helperHeight }) => ({
+        "root": {
+            "backgroundColor": "inherit",
+            "paddingBottom": theme.spacing(5),
+        },
+        "title": {
+            "display": "flex",
+            "alignItems": "center",
+        },
+        "titleIcon": {
+            "marginRight": theme.spacing(3),
+        },
+        "helperRoot": {
+            "display": "flex",
+            "backgroundColor": theme.colors.useCases.surfaces.surface2,
+            "alignItems": "start",
+            "padding": theme.spacing(3),
+            "borderRadius": helperHeight * 0.15,
+            "marginTop": theme.spacing(3),
+        },
+        "helperMiddle": {
+            "flex": 1,
+        },
+        "helperIcon": {
+            "marginRight": theme.spacing(3),
+            "color": theme.colors.useCases.typography.textFocus,
+        },
+        "closeButton": {
+            "padding": 0,
+            "marginLeft": theme.spacing(3),
+        },
+    }),
+);
+
+const { usePageHeaderClosedHelpers } = createUseGlobalState(
+    "pageHeaderClosedHelpers",
+    id<string[]>([]),
+    { "persistance": false },
+);
+
+const { IconButton } = createIconButton(createIcon({ "close": CloseSharp }));
+
+export function createPageHeader<IconId extends string>(params?: {
+    Icon(props: IconProps<IconId>): ReturnType<FC>;
+}) {
+    const { Icon } = params ?? {
+        "Icon": id<(props: IconProps<IconId>) => JSX.Element>(() => {
+            throw new Error("never");
+        }),
+    };
+
+    const PageHeader = memo((props: PageHeaderProps<IconId>) => {
+        const { mainIcon, title, helpTitle, helpIcon, helpContent, className } =
+            props;
+
+        const {
+            ref: helperRef,
+            domRect: { height: helperHeight },
+        } = useDomRect();
+
+        const { classes, cx } = useStyles({ helperHeight });
+
+        const { isHelpShown, hideHelp } = (function useClosure() {
+            const { pageHeaderClosedHelpers, setPageHeaderClosedHelpers } =
+                usePageHeaderClosedHelpers();
+
+            const isHelpShown = !pageHeaderClosedHelpers.includes(title);
+
+            const hideHelp = useConstCallback(() =>
+                setPageHeaderClosedHelpers([...pageHeaderClosedHelpers, title]),
+            );
+
+            return { isHelpShown, hideHelp };
+        })();
+
+        return (
+            <div className={cx(classes.root, className)}>
+                <Text typo="page heading" className={classes.title}>
+                    {mainIcon && (
+                        <Icon
+                            iconId={mainIcon}
+                            className={classes.titleIcon}
+                            size="large"
+                        />
+                    )}
+                    {title}
+                </Text>
+                {isHelpShown && (
+                    <div ref={helperRef} className={classes.helperRoot}>
+                        {helpIcon && (
+                            <div>
+                                <Icon
+                                    iconId={helpIcon}
+                                    className={classes.helperIcon}
+                                />
+                            </div>
+                        )}
+                        <div className={classes.helperMiddle}>
+                            <Text typo="navigation label">{helpTitle}</Text>
+                            <Text typo="body 1">{helpContent}</Text>
+                        </div>
+                        <div>
+                            <IconButton
+                                iconId="close"
+                                onClick={hideHelp}
+                                className={classes.closeButton}
+                            />
+                        </div>
+                    </div>
+                )}
+            </div>
+        );
+    });
+
+    return { PageHeader };
+}
---
+++ src/SearchBar.tsx
@@ -0,0 +1,195 @@
+import { useRef, useState, memo } from "react";
+import type { ChangeEventHandler } from "react";
+import { makeStyles } from "./lib/ThemeProvider";
+import { useConstCallback } from "powerhooks/useConstCallback";
+import { useClickAway } from "powerhooks/useClickAway";
+import type { NonPostableEvt } from "evt";
+import { useEvt } from "evt/hooks";
+import { createIcon } from "./Icon";
+import { createIconButton } from "./IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+import CancelIcon from "@material-ui/icons/Cancel";
+
+const { Icon } = createIcon({
+    "search": SearchIcon,
+    "cancel": CancelIcon,
+});
+
+const { IconButton } = createIconButton({ Icon });
+
+export type SearchBarProps = {
+    className?: string;
+    search: string;
+    onSearchChange(search: string): void;
+    evtAction?: NonPostableEvt<"CLEAR SEARCH">;
+    /** Default "Search" */
+    placeholder?: string;
+};
+
+const useStyles = makeStyles<{ isActive: boolean }>()(
+    (theme, { isActive }) => ({
+        "root": {
+            "borderRadius": 8,
+            "overflow": "hidden",
+            "boxShadow": theme.shadows[1],
+            "& > div": {
+                "display": "flex",
+                "alignItems": "center",
+                "backgroundColor": theme.colors.useCases.surfaces.surface1,
+                "cursor": isActive ? undefined : "pointer",
+                "overflow": "hidden",
+                "border": "solid 2px transparent",
+                "&:hover": {
+                    "borderBottomColor":
+                        theme.colors.useCases.buttons.actionActive,
+                },
+            },
+        },
+        "input": {
+            "flex": 1,
+            "caretColor": theme.colors.useCases.typography.textFocus,
+            ...theme.typography.variants["body 1"].style,
+            "outline": "none",
+            "borderWidth": 0,
+            "border": "none",
+            "backgroundColor": "transparent",
+            "color": theme.colors.useCases.typography.textPrimary,
+            "&::placeholder": {
+                "color": theme.colors.useCases.typography.textDisabled,
+                "opacity": 1,
+            },
+        },
+        "icon": {
+            "margin": `${theme.spacing(2) - 2}px ${theme.spacing(3) - 2}px`,
+            "color": isActive
+                ? theme.colors.useCases.typography.textFocus
+                : undefined,
+        },
+        "searchLabel": {
+            ...(theme.muiTheme.typography.button as any),
+            "display": "block",
+            "flex": 1,
+            "color": theme.colors.useCases.typography.textPrimary,
+        },
+    }),
+);
+
+export const SearchBar = memo((props: SearchBarProps) => {
+    const {
+        className,
+        onSearchChange,
+        search,
+        placeholder = "Search",
+        evtAction,
+    } = props;
+
+    const [isActive, setIsActive] = useState(search !== "");
+
+    const { classes, cx } = useStyles({ isActive });
+
+    const onClearButtonClick = useConstCallback(() => {
+        onSearchChange("");
+        inputRef.current?.focus();
+    });
+    const onRootClick = useConstCallback(() => setIsActive(true));
+    const onIconClick = useConstCallback(() => {
+        const { current: inputEl } = inputRef;
+        if (inputEl === null) return;
+        inputEl.focus();
+        inputEl.setSelectionRange(0, search.length);
+    });
+    const onInputChange = useConstCallback<
+        ChangeEventHandler<HTMLInputElement>
+    >(event => {
+        const { value } = event.target;
+        onSearchChange(value);
+    });
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const onInputKeyDown = useConstCallback((event: { key: string }) => {
+        const key = (() => {
+            switch (event.key) {
+                case "Escape":
+                case "Enter":
+                    return event.key;
+                default:
+                    return "irrelevant";
+            }
+        })();
+
+        if (key === "irrelevant") {
+            return;
+        }
+
+        switch (key) {
+            case "Enter":
+                if (search === "") {
+                    setIsActive(false);
+                }
+                break;
+            case "Escape":
+                onSearchChange("");
+                setIsActive(false);
+                break;
+        }
+
+        inputRef.current?.blur();
+    });
+
+    const { rootRef } = useClickAway(() => {
+        if (search !== "") return;
+        setIsActive(false);
+    });
+
+    useEvt(
+        ctx =>
+            evtAction?.attach(
+                action => action === "CLEAR SEARCH",
+                ctx,
+                () => onInputKeyDown({ "key": "Escape" }),
+            ),
+        [evtAction ?? Object],
+    );
+
+    return (
+        <div
+            ref={rootRef}
+            className={cx(classes.root, className)}
+            onClick={onRootClick}
+        >
+            <div>
+                <Icon
+                    iconId="search"
+                    onClick={onIconClick}
+                    className={classes.icon}
+                />
+                {isActive ? (
+                    <>
+                        <input
+                            ref={inputRef}
+                            autoFocus={true}
+                            className={classes.input}
+                            type="text"
+                            value={search}
+                            onChange={onInputChange}
+                            onKeyDown={onInputKeyDown}
+                            spellCheck={false}
+                            placeholder={placeholder}
+                        />
+                        {
+                            <IconButton
+                                iconId="cancel"
+                                size="small"
+                                disabled={search === ""}
+                                onClick={onClearButtonClick}
+                            />
+                        }
+                    </>
+                ) : (
+                    <span className={classes.searchLabel}>{placeholder}</span>
+                )}
+            </div>
+        </div>
+    );
+});
