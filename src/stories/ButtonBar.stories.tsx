7842bd2eed46213e28b212be24eef32989bd5819
garronej <joseph.garrone.gj@gmail.com>
2021-08-24 18:29:08 +0200

    Add missing files

---
+++ src/stories/ButtonBar.stories.tsx
@@ -0,0 +1,45 @@
+import { createButtonBar } from "../ButtonBar";
+import { sectionName } from "./sectionName";
+import { getStoryFactory, logCallbacks } from "./getStory";
+import { Icon } from "./theme";
+
+const { ButtonBar } = createButtonBar({
+    "Icon": Icon,
+});
+
+const { meta, getStory } = getStoryFactory({
+    sectionName,
+    "wrappedComponent": { ButtonBar },
+});
+
+export default meta;
+
+export const VueDefault = getStory({
+    "buttons": [
+        {
+            "buttonId": "btn1",
+            "icon": "help",
+            "isDisabled": false,
+            "label": "Label 1",
+        },
+        {
+            "buttonId": "btn2",
+            "icon": "home",
+            "isDisabled": false,
+            "label": "Label 2",
+        },
+        {
+            "buttonId": "btn3",
+            "icon": "services",
+            "isDisabled": true,
+            "label": "Label 3",
+        },
+        {
+            "buttonId": "btn4",
+            "icon": "tour",
+            "isDisabled": false,
+            "label": "Label 4",
+        },
+    ] as const,
+    ...logCallbacks(["onClick"]),
+});
