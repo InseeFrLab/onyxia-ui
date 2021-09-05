44b088cc105b0365894a5263a6e44a8ab838127f
garronej <joseph.garrone.gj@gmail.com>
2021-09-05 03:28:14 +0200

    Add story for search bar

---
+++ src/stories/SearchBar.stories.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { SearchBar } from "../SearchBar";
+import type { SearchBarProps } from "../SearchBar";
+import { sectionName } from "./sectionName";
+import { getStoryFactory, logCallbacks } from "./getStory";
+import { symToStr } from "tsafe/symToStr";
+
+function Component(props: Omit<SearchBarProps, "search" | "onSearchChange">) {
+    const [search, setState] = useState("");
+
+    return <SearchBar search={search} onSearchChange={setState} {...props} />;
+}
+
+const { meta, getStory } = getStoryFactory({
+    sectionName,
+    "wrappedComponent": { [symToStr({ SearchBar })]: Component },
+    "defaultWidth": 700,
+});
+
+export default meta;
+
+export const VueDefault = getStory({
+    ...logCallbacks(["onKeyPress"]),
+});
