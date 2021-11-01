0d0b94681623230c53b51ef06332aafb43ff0934
garronej <joseph.garrone.gj@gmail.com>
2021-11-01 18:52:41 +0100

    Improve type def of TextField autoComplete

--- src/TextField.tsx
+++ src/TextField.tsx
@@ -25,7 +25,6 @@ export type TextFieldProps = {
     className?: string | null;
     id?: string | null;
     name?: string | null;
-    autoComplete?: "on" | "off";
     type?: "text" | "password" | "email";
     /** Will overwrite value when updated */
     defaultValue?: string;
@@ -71,6 +70,61 @@ export type TextFieldProps = {
     doOnlyValidateInputAfterFistFocusLost?: boolean;
     isCircularProgressShown?: boolean;
     selectAllTextOnFocus?: boolean;
+    autoComplete?:
+        | "on"
+        | "off"
+        | "name"
+        | "honorific-prefix"
+        | "given-name"
+        | "additional-name"
+        | "family-name"
+        | "honorific-suffix"
+        | "nickname"
+        | "email"
+        | "username"
+        | "new-password"
+        | "current-password"
+        | "one-time-code"
+        | "organization-title"
+        | "organization"
+        | "street-address"
+        | "address-line1"
+        | "address-line2"
+        | "address-line3"
+        | "address-level4"
+        | "address-level3"
+        | "address-level2"
+        | "address-level1"
+        | "country"
+        | "country-name"
+        | "postal-code"
+        | "cc-name"
+        | "cc-given-name"
+        | "cc-additional-name"
+        | "cc-family-name"
+        | "cc-number"
+        | "cc-exp"
+        | "cc-exp-month"
+        | "cc-exp-year"
+        | "cc-csc"
+        | "cc-type"
+        | "transaction-currency"
+        | "transaction-amount"
+        | "language"
+        | "bday"
+        | "bday-day"
+        | "bday-month"
+        | "bday-year"
+        | "sex"
+        | "tel"
+        | "tel-country-code"
+        | "tel-national"
+        | "tel-area-code"
+        | "tel-local"
+        | "tel-extension"
+        | "impp"
+        | "url"
+        | "photo";
 };
 
 const { Icon } = createIcon({
