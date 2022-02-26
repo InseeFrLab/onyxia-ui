295439e87e9ab79afd8a804b74b52367df888ea5
Joseph Garrone <joseph.garrone.gj@gmail.com>
2022-02-26 23:08:26 +0100

    Update onyxia-init.sh

--- onyxia-init.sh
+++ onyxia-init.sh
@@ -2,3 +2,4 @@
 echo "Hello World" > /home/coder/work/onyxia-ui/hello.txt
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
+sudo npm install -g yarn
