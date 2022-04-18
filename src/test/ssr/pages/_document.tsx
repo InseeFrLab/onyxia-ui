9613661bded05694d910678893d6c630648a6a18
garronej <joseph.garrone.gj@gmail.com>
2022-04-18 16:35:09 +0200

    Remove test SPA app

--- src/test/ssr/pages/_document.tsx
+++
@@ -1,6 +0,0 @@
-import { createDocument } from "tss-react/nextJs";
-import { muiCache } from "../shared/theme";
-
-const { Document } = createDocument({ "caches": [muiCache] });
-
-export default Document;
