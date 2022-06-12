6f63e57a65118d470441e659707efa017b505284
garronej <joseph.garrone.gj@gmail.com>
2022-06-13 01:16:54 +0200

    Export some types from Icon

--- src/Icon.tsx
+++ src/Icon.tsx
@@ -40,13 +40,13 @@ export type IconProps<IconId extends string = string> = {
     onClick?: MouseEventHandler<SVGSVGElement>;
 };
 
-type MuiIconLike = (props: {
+export type MuiIconLike = (props: {
     ref: ForwardedRef<SVGSVGElement>;
     className: string;
     onClick?: MouseEventHandler<SVGSVGElement>;
 }) => JSX.Element;
 
-type SvgComponentLike = ElementType;
+export type SvgComponentLike = ElementType;
 
 function isMuiIcon(
     Component: MuiIconLike | SvgComponentLike,
