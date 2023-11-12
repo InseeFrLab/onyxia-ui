0ad0da2403be10f023216fca1b8bb542810e2d3e
Joseph Garrone <joseph.garrone.gj@gmail.com>
2023-11-12 14:28:52 +0100

    Update Markdown component

--- src/Markdown.tsx
+++ src/Markdown.tsx
@@ -1,43 +1,81 @@
-import { memo, useMemo } from "react";
-import type { ReactNode } from "react";
+import { memo, useMemo, createElement } from "react";
 import ReactMarkdown from "react-markdown";
-import Link from "@mui/material/Link";
+import MuiLink from "@mui/material/Link";
+import { symToStr } from "tsafe/symToStr";
 
-export type MarkdownProps = {
+export type Props = {
     className?: string;
     children: string;
-    getDoesLinkShouldOpenNewTab?: (href: string) => boolean;
+    getLinkProps?: (params: {
+        href: string;
+    }) => React.ComponentProps<typeof MuiLink>;
+    /** Default: false */
+    allowHtml?: boolean;
+    /** Default: false */
+    inline?: boolean;
+    /** For accessibility only */
+    lang?: string;
 };
 
-export const Markdown = memo((props: MarkdownProps) => {
+export const Markdown = memo((props: Props) => {
     const {
         className,
-        getDoesLinkShouldOpenNewTab = () => false,
         children,
+        getLinkProps = ({ href }): React.ComponentProps<typeof MuiLink> => ({
+            href,
+            "target": href.startsWith("http") ? "_blank" : undefined,
+        }),
+        inline: isInline = false,
+        allowHtml: doAllowHtml = false,
+        lang,
     } = props;
 
     const renderers = useMemo(
         () => ({
-            "link": (props: { href: string; children: ReactNode }) => (
-                <Link
-                    underline="hover"
-                    href={props.href}
-                    target={
-                        getDoesLinkShouldOpenNewTab(props.href)
-                            ? "_blank"
-                            : undefined
-                    }
-                >
-                    {props.children}
-                </Link>
-            ),
+            "link": ({
+                href,
+                children,
+            }: {
+                children: React.ReactNode;
+                href: string;
+            }) => <MuiLink {...getLinkProps({ href })}>{children}</MuiLink>,
+            "paragraph": ({ children }: { children: React.ReactNode }) => {
+                console.log(props);
+                const as = isInline ? "span" : "p";
+                return createElement(as, {
+                    children,
+                    lang,
+                });
+            },
         }),
-        [],
+        [getLinkProps, isInline],
     );
 
     return (
-        <ReactMarkdown className={className} renderers={renderers}>
+        <ReactMarkdown
+            allowDangerousHtml={doAllowHtml}
+            className={className}
+            renderers={renderers}
+        >
             {children}
         </ReactMarkdown>
     );
 });
+
+Markdown.displayName = symToStr({ Markdown });
+
+export function createMarkdown(params: {
+    getLinkProps: (params: {
+        href: string;
+    }) => React.ComponentProps<typeof MuiLink>;
+}) {
+    const { getLinkProps } = params;
+
+    const MarkdownWithLinkRenderer = (props: Omit<Props, "renderLink">) => (
+        <Markdown getLinkProps={getLinkProps} {...props} />
+    );
+
+    MarkdownWithLinkRenderer.displayName = Markdown.displayName;
+
+    return { "Markdown": MarkdownWithLinkRenderer };
+}
