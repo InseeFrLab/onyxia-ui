b5d0ad80c5d351554526729e1cb8a9b10db80f52
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-01-09 18:51:45 +0100

    Add scrolling test

--- test-app/src/TextFormDialog.tsx
+++ test-app/src/TextFormDialog.tsx
@@ -73,6 +73,24 @@ export const TextFormDialog = memo((props: TextFormDialogProps) => {
                                 setOpenState({ ...openState, "text": value });
                             }}
                         />
+
+                        {/*
+                            new Array(10).fill(null).map((_, index) => (
+                                <p key={index} >
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua.
+                                    Ut enim ad minim veniam, quis nostrud
+                                    exercitation ullamco laboris nisi ut
+                                    aliquip ex ea commodo consequat. Duis aute
+                                    irure dolor in reprehenderit in voluptate
+                                    velit esse cillum dolore eu fugiat nulla
+                                    pariatur. Excepteur sint occaecat cupidatat
+                                    non proident, sunt in culpa qui officia
+                                    deserunt mollit anim id est laborum.
+                                </p>
+                            ))
+                            */}
                     </>
                 )
             }
