9bb40b4f5926c12d7591d46948ca68fc465bb2ec
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-02-01 17:43:25 +0100

    Actually support file:/

--- src/tools/getSafeUrl.ts
+++ src/tools/getSafeUrl.ts
@@ -1,10 +1,14 @@
 export function getSafeUrl(url: string) {
+    if (url.startsWith("file://")) {
+        return url;
+    }
+
     let unsafeUrl = url;
     let toReturn = url;
 
     if (url.startsWith("/")) {
         unsafeUrl = `${window.location.origin}${url}`;
-    } else if (!/^[^a-zA-Z0-9]+:\/\//.test(url)) {
+    } else if (!url.startsWith("http")) {
         unsafeUrl = `https://${url}`;
         toReturn = unsafeUrl;
     }
